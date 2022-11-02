64a70ef2c187e02520dcba2d86d6c3905c0c7a8b
573NED <573ned@gmail.com>
2022-11-03 00:09:02 +0300

    task5 complete

---
+++ lab5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n, k, i;
+    cout << "введите количество чисел : ";
+    cin >> n;
+    int a[n]; // обозначение массива чисел
+    cout << "перечислите числа через пробел : ";
+    for(i = 0; i < n; i++)  
+        cin >> a[i]; // ввод массива
+    cout << "введите шаг сдвига : ";
+    cin >> k;
+
+    for(int i = 0; i <= k; i++){
+        int t;
+        t = a[n-1];
+        for (int j = n-2; j>=0; j--)
+            a[j+1] = a[j];
+        a[0] = t;
+    }
+
+    for(i = 0; i < n; i ++)  
+        cout <<a[i]<<" "; // вывод массива
+}
